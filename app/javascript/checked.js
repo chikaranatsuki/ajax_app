2520823bbba1d7f7607ec8d7c57689198f4c109e
chikaranatsuki <69283318+chikaranatsuki@users.noreply.github.com>
2020-09-19 18:40:28 +0900

    既読機能のフロントエンド実装

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,25 +1,38 @@
 function check() {
+  // 表示されているすべてのメモを取得している
   const posts = document.querySelectorAll(".post");
-  posts.forEach(function (post) { 
-    if (post.getAttribute("data-load") != null) {
+  posts.forEach(function (post) {
+     if (post.getAttribute("data-load") != null) {
       return null;
     }
     post.setAttribute("data-load", "true");
-    post.addEventListener("click", () => { 
-      const postId = post.getAttribute("data-id"); //getAttributeで属性値を取得することができるので、メモのidを取得する
-      const XHR = new XMLHttpRequest(); //オブジェクトを生成する
-      XHR.open("GET", `/posts/${postId}`, true);  //どのようなリクエストをするのかを指定するメソッド。リクエストの初期化。
-      XHR.responseType = "json";  //レスポンスとして欲しい情報の形式を指定するメソッド
-      XHR.send(); //XMLHttpRequestで定義されているメソッドで、リクエストを送信できます
+    // メモをクリックした場合に実行する処理を定義している
+    post.addEventListener("click", () => {
+      // どのメモをクリックしたのか、カスタムデータを利用して取得している
+      const postId = post.getAttribute("data-id");
+      // Ajaxに必要なオブジェクトを生成している
+      const XHR = new XMLHttpRequest();
+      // openでリクエストを初期化する
+      XHR.open("GET", `/posts/${postId}`, true);
+      // レスポンスのタイプを指定する
+      XHR.responseType = "json";
+      // sendでリクエストを送信する
+      XHR.send();
+      // レスポンスを受け取った時の処理を記述する
       XHR.onload = () => {
         if (XHR.status != 200) {
+          // レスポンスの HTTP ステータスを解析し、該当するエラーメッセージをアラートで表示するようにしている
           alert(`Error ${XHR.status}: ${XHR.statusText}`);
+          // 処理を終了している
           return null;          
-        }                                    //レスポンスがエラーだった場合の処理
+        }
+        // レスポンスされたデータを変数itemに代入している
         const item = XHR.response.post;
         if (item.checked === true) {
+          // 既読状態であれば、灰色に変わるcssを適用するためのカスタムデータを追加している
           post.setAttribute("data-check", "true");
         } else if (item.checked === false) {
+          // 未読状態であれば、カスタムデータを削除している
           post.removeAttribute("data-check");
         }
       };
