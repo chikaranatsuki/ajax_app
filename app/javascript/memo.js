7c742614a5c000c2097863b34419f300bb059fcc
chikaranatsuki <69283318+chikaranatsuki@users.noreply.github.com>
2020-10-20 12:25:14 +0900

    Revert "復習"
    This reverts commit e2b5715112c8c901ff01be4f91e5bd157ef89207.

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,17 +1,12 @@
 
 function memo() {
   const submit = document.getElementById("submit");
-     // getElementById「投稿する」ボタンの情報を取得
   submit.addEventListener("click", (e) => {
     const formData = new FormData(document.getElementById("form"));
-    // フォームに入力された値を取得できるオブジェクト。new FormData(フォームの要素);
     const XHR = new XMLHttpRequest();
-    // 非同期通信を実装するために必要なXMLHttpRequestのオブジェクトを生成
     XHR.open("POST", "/posts", true);
-    // open＝初期化（リクエストが動き出すスタート）（HTTPメソッド,パス,非同期通信）
     XHR.responseType = "json";
     XHR.send(formData);
-    // メモ投稿のフォーム（formdata)に入力された情報を送信
     XHR.onload = () => {
       if (XHR.status != 200) {
         alert(`Error ${XHR.status}: ${XHR.statusText}`);
@@ -30,11 +25,9 @@ function memo() {
           </div>
         </div>`;
       list.insertAdjacentHTML("afterend", HTML);
-      // 指定したHTMLなどを、特定の要素に描画できるメソッド(挿入する)
       formText.value = "";
     };
     e.preventDefault();
   });
  }
  window.addEventListener("load", memo);
-//  window（ページ）をload（読み込み）した時に実行します。
